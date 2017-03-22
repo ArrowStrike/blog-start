6819b5828940469c9e9b0b714c30c4e798b85a5e
Vlad <kawasak2@gmail.com>
2017-03-22 22:20:26 +0200

    redirect

--- admin/authorization.php
+++ admin/authorization.php
@@ -12,14 +12,12 @@ $link = db_connect();
 $email = htmlspecialchars($_POST["email"]);
 /*if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo "E-mail ($email) указан верно.\n";
-} else {
-    echo "E-mail ($email) указан неверно.\n";
-}*/
+} */
 $password = htmlspecialchars($_POST["password"]);
 //$password = password_hash($password, PASSWORD_DEFAULT);
 if (checkUser($link, $email, $password)) {
     $_SESSION["email"] = $email;
     $_SESSION["password"] = $password;
 } else $_SESSION["error_auth"] = 1;
-header("Location: " . $_SERVER["HTTP_REFERER"]);
+redirect($_SERVER["HTTP_REFERER"]);
 exit;
