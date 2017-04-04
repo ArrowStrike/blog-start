1048bdbff9f49dff5dba0653d07f3a3335fa142d
Vladislav <kawasak2@gmail.com>
2017-04-04 20:05:22 +0300

    new hoster ambiweb.lv

--- includes/links.php
+++ includes/links.php
@@ -4,7 +4,7 @@ require "config.php";
 <!-- Bootstrap Grid -->
 <link rel="stylesheet"
       type="text/css"
-      href="http://blog.com.host1565037.serv68.hostland.pro/media/assets/bootstrap-grid-only/css/grid12.css">
+      href="http://blog.ambiweb.lv/media/assets/bootstrap-grid-only/css/grid12.css">
 
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"
@@ -13,7 +13,7 @@ require "config.php";
 <!-- Custom -->
 <link rel="stylesheet"
       type="text/css"
-      href="http://blog.com.host1565037.serv68.hostland.pro/media/css/style.css?version=<?php echo $config['version'] ?>">
+      href="http://blog.ambiweb.lv/media/css/style.css?version=<?php echo $config['version'] ?>">
 <link rel="shortcut icon"
-      href="http://blog.com.host1565037.serv68.hostland.pro/media/images/books.ico"
+      href="http://blog.ambiweb.lv/media/images/books.ico"
       type="image/x-icon">
